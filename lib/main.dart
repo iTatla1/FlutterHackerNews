bc80cbc434e078e6fe56b5e9596303d54788b0a6
Muhammad Usman <usm.gh36@gmail.com>
2020-07-18 13:21:28 +0500

    HackerNews Final Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'dart:collection';
 
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:hackernews/model/article.dart';
 import 'package:hackernews/src/hn_bloc.dart';
 import 'package:url_launcher/url_launcher.dart';
+
+import 'src/hn_bloc.dart';
 
 void main() async {
   final hnBloc = HackerNewsBloc();
@@ -45,21 +48,62 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   List<Article> articles = [];
+  int selectedTab = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
-      ),
-      body: StreamBuilder<UnmodifiableListView<Article>>(
-        stream: widget.bloc.articles,
-        initialData: UnmodifiableListView<Article>([]),
-        builder: (context, snapshot) => ListView(
-          children: snapshot.data.map(_buildItem).toList(),
+        leading: LoadingInfo(
+          loadingStream: widget.bloc.isLoading,
         ),
       ),
+      body: StreamBuilder<UnmodifiableListView<Article>>(
+          stream: widget.bloc.articles,
+          initialData: UnmodifiableListView<Article>([]),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              Text("Some server error happened");
+            }
+            if (!snapshot.hasData) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            if (snapshot.hasData && snapshot.data.length > 0) {
+              return ListView(
+                children: snapshot.data.map(_buildItem).toList(),
+              );
+            }
+            return Container();
+          }),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: selectedTab,
+        onTap: (value) {
+          setState(() {
+            selectedTab = value;
+          });
+          widget.bloc.storiesType.add(
+              value == 0 ? StoriesType.topStories : StoriesType.newStories);
+        },
+        items: [
+          BottomNavigationBarItem(
+            title: Text("Top Stories"),
+            icon: Icon(Icons.star),
+          ),
+          BottomNavigationBarItem(
+            title: Text("New Stories"),
+            icon: Icon(Icons.new_releases),
+          )
+        ],
+      ),
     );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
   }
 
   Widget _buildItem(Article article) => Padding(
@@ -101,5 +145,60 @@ class _MyHomePageState extends State<MyHomePage> {
     } else {
       debugPrint("Can not launch the url $fUrl");
     }
+  }
+}
+
+class LoadingInfo extends StatefulWidget {
+  final Stream<bool> loadingStream;
+
+  const LoadingInfo({Key key, this.loadingStream}) : super(key: key);
+
+  @override
+  _LoadingInfoState createState() => _LoadingInfoState();
+}
+
+class _LoadingInfoState extends State<LoadingInfo>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+//  Animation<double> _opacity;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(
+        seconds: 3,
+      ),
+    );
+
+//    _opacity = Tween<double>(begin: 0.0, end: 1.0).animate(_controller);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: widget.loadingStream,
+      builder: (BuildContext context, AsyncSnapshot<bool> asyncShot) {
+        if (asyncShot.hasData && asyncShot.data) {
+          _controller.forward();
+        } else {
+          _controller.reverse();
+        }
+        return FadeTransition(
+          opacity: _controller,
+          child: Icon(
+            FontAwesomeIcons.hackerNewsSquare,
+            color: Colors.orange,
+          ),
+        );
+      },
+    );
   }
 }
--- lib/src/hn_bloc.dart
+++ lib/src/hn_bloc.dart
@@ -5,29 +5,61 @@ import 'package:hackernews/model/article.dart';
 import 'package:http/http.dart' as http;
 import 'package:rxdart/rxdart.dart';
 
+enum StoriesType { topStories, newStories }
+
 class HackerNewsBloc {
   Stream<List<Article>> get articles => _articlesSubject.stream;
   final _articlesSubject = BehaviorSubject<UnmodifiableListView<Article>>();
 
+  Stream<bool> get isLoading => _isLoadingSubject.stream;
+  final _isLoadingSubject = BehaviorSubject.seeded(false);
+
   var _articles = <Article>[];
 
-  List<int> articleIds = [
+  Sink<StoriesType> get storiesType => _storiesTypeController.sink;
+  final _storiesTypeController = StreamController<StoriesType>();
+
+  void closeStreams() {
+    _storiesTypeController.close();
+    _articlesSubject.close();
+    _isLoadingSubject.close();
+  }
+
+  static List<int> _newIds = [
+    23825979,
+    23828499,
+    23823963,
+  ];
+
+  static List<int> _topIds = [
     23828253,
     23826176,
     23828475,
     23828920,
-    23825979,
-    23828499,
-    23823963
   ];
 
   HackerNewsBloc() {
-    _updateArticles().then((_) {
-      _articlesSubject.add(UnmodifiableListView(_articles));
+    _getArticlesAndUpdate(_topIds);
+
+    _storiesTypeController.stream.listen((storiesType) {
+      List<int> ids;
+      if (storiesType == StoriesType.newStories) {
+        ids = _newIds;
+      } else {
+        ids = _topIds;
+      }
+      _getArticlesAndUpdate(ids);
     });
   }
 
-  Future<Null> _updateArticles() async {
+  _getArticlesAndUpdate(List<int> ids) async {
+    _isLoadingSubject.add(true);
+    await _updateArticles(ids);
+    _articlesSubject.add(UnmodifiableListView(_articles));
+    _isLoadingSubject.add(false);
+  }
+
+  Future<Null> _updateArticles(List<int> articleIds) async {
     final futureArticles = articleIds.map((id) => getArticle(id));
     final articles = await Future.wait(futureArticles);
     _articles = articles;
